ba4ba6e215117583e6b91818b414be9b663370b1
Robert Wilson <youmufan@gmail.com>
2018-02-06 20:18:27 -0500

    rewrote the function to used named functions for click handlers

--- src/App.js
+++ src/App.js
@@ -6,12 +6,26 @@ class App extends Component {
     this.state = {
       count:0
     }
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    /*
+    Binding means inside of "increment" the "this" keyword is the component
+    */
   }
+
+  increment() {
+    this.setState({count: this.state.count + 1});
+  }
+
+  decrement() {
+    this.setState({count: this.state.count - 1});
+  }
+
   render() {
     return (
       <div>
-        <button onClick={() => this.setState({count: this.state.count +1})}>+</button>
-        <button onClick={() => this.setState({count: this.state.count -1})}>-</button>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
         <h1>{this.state.count}</h1>
       </div>
     );
